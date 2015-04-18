7a57e780509867e49296666264c3e61ad8762f0e
Alex Kersten <alex@kersten.email>
2015-04-18 15:32:07 -0500

    add some more base gameobject fields

--- core/src/com/bitshifting/entities/GameObject.java
+++ core/src/com/bitshifting/entities/GameObject.java
@@ -1,13 +1,23 @@
 package com.bitshifting.entities;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
 public abstract class GameObject {
     public Vector2 position;
+    public Vector2 velocity;
 
-    public GameObject(Vector2 position)
-    {
+    Texture texture;
+    float rotation;
+
+
+    public GameObject(Vector2 position)   {
         this.position = position;
+        this.texture = new Texture(Gdx.files.internal("assets/tito.png"));
+        this.rotation = 0.0f;
+
+
     }
 
     public abstract void update(float dt);
