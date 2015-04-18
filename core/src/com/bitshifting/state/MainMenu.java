ccc82213fc2d6057f372881fe673c6e4d01d71f9
Ricardo Lopez <rlopez4@wisc.edu>
2015-04-18 16:06:27 -0500

    implement test button

--- core/src/com/bitshifting/state/MainMenu.java
+++ core/src/com/bitshifting/state/MainMenu.java
@@ -1,10 +1,47 @@
 package com.bitshifting.state;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.bitshifting.managers.StateManager;
 
 public class MainMenu extends State {
+
+    Stage stage = new Stage();
+    TextButton button;
+    TextButton.TextButtonStyle textButtonStyle;
+    BitmapFont font;
+    Skin skin;
+    TextureAtlas buttonAtlas;
+
     public MainMenu(StateManager sm) {
         super(sm);
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+        skin = new Skin();
+        font = new BitmapFont();
+        buttonAtlas = new TextureAtlas(Gdx.files.internal("Button/ButtonPack.atlas"));
+        skin.addRegions(buttonAtlas);
+        textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.up = skin.getDrawable("Play");
+        textButtonStyle.over = skin.getDrawable("PlayHover");
+        textButtonStyle.down = skin.getDrawable("PlayClick");
+
+        button = new TextButton("", textButtonStyle);
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked (InputEvent event, float x, float y) {
+                System.out.println("Button Clicked.");
+            }
+        });
+        stage.addActor(button);
     }
 
     public void handleInput() {
@@ -16,7 +53,7 @@ public class MainMenu extends State {
     }
 
     public void render() {
-
+        stage.draw();
     }
 
     public void dispose() {
