12181e8822d29badde8cf1e05aa026ac9a3a5e2a
Kenneth S <kksiu@wisc.edu>
2015-04-18 23:10:44 -0500

    Added choose and rules state

---
+++ core/src/com/bitshifting/state/ChooseState.java
@@ -0,0 +1,30 @@
+package com.bitshifting.state;
+
+import com.bitshifting.managers.StateManager;
+
+public class ChooseState extends State {
+
+    public ChooseState(StateManager sm) {
+        super(sm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
